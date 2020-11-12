cb41a7bf780e40202fb67a702ceca435dc45b46d
Stefan Oehrli <stefan.oehrli@trivadis.com>
2020-11-12 06:31:14 +0100

    add skeletons for admin scripts

---
+++ bin/tns_dump.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------
+# Trivadis AG, Infrastructure Managed Services
+# Saegereistrasse 29, 8152 Glattbrugg, Switzerland
+# ---------------------------------------------------------------------------
+# Name.......: tns_dump.sh
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@trivadis.com
+# Editor.....: Stefan Oehrli
+# Date.......: 2020.11.10
+# Revision...: 
+# Purpose....: Dump entries as tnsnames.ora
+# Notes......: --
+# Reference..: --
+# License....: Apache License Version 2.0, January 2004 as shown
+#              at http://www.apache.org/licenses/
+# ---------------------------------------------------------------------------
+# Define a bunch of bash option see 
+# https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
+set -o nounset          # stop script after 1st cmd failed
+set -o errexit          # exit when 1st unset variable found
+set -o pipefail         # pipefail exit after 1st piped commands failed
+# - Customization -----------------------------------------------------------
+# - just add/update any kind of customized environment variable here
+
+# - End of Customization ----------------------------------------------------
+
+# - Environment Variables ---------------------------------------------------
+# slapd generic configuration
+
+# - EOF Environment Variables -----------------------------------------------
+
+# - Functions ---------------------------------------------------------------
+function command_exists () {
+# Purpose....: check if a command exists. 
+# -----------------------------------------------------------------------
+    command -v $1 >/dev/null 2>&1;
+}
+# - EOF Functions -----------------------------------------------------------
+
+# - Initialization ----------------------------------------------------------
+# check if we do have a openssl
+for i in openssl pwgen envsubst; do
+    if ! command_exists ${i}; then
+        echo "ERR : Command ${i} isn't installed/available on this system..."
+        exit 1
+    fi
+done
+# - EOF Initialization -------------------------------------------------------
+ 
+# - Main ---------------------------------------------------------------------
+
+# --- EOF --------------------------------------------------------------------
